2f3ab43750f76b6aed2dba7a565b9990ebfbc831
JordanPK <jordanpk7@gmail.com>
2026-01-07 12:53:43 -0500

    Add countries entity

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,19 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi(pattern:"swagger/openapi/v1.json");
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/openapi/v1.json", "Orders API v1")); 
-
+    app.MapOpenApi(pattern: "swagger/openapi/v1.json");
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/openapi/v1.json", "Orders API v1"));
 }
 
 app.UseHttpsRedirection();
